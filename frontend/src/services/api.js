746306078efa32274d5d0f5da88648cedf885945
Sreejith S <sreejiths2102@gmail.com>
2026-07-02 20:12:35 +0530

    changes

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,12 +1,13 @@
 import axios from "axios"
 
-const api=axios.create({
-    baseURL:"http://127.0.0.1:8000",
+const api = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+    withCredentials: true,
 });
-api.interceptors.request.use((config)=>{
-    const token=localStorage.getItem("token");
-    if (token){
-        config.headers.Authorization=`Bearer ${token}`;
+api.interceptors.request.use((config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
 }
